b5cc1919647bd6543808a3b27a6508095ca98765
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-08-05 15:36:08 -0300

    feat(section): section repository

--- src/main/java/com/mercadolibre/joao_magalhaes/domain/repository/SectionRepository.java
+++ src/main/java/com/mercadolibre/joao_magalhaes/domain/repository/SectionRepository.java
@@ -2,9 +2,11 @@ package com.mercadolibre.joao_magalhaes.domain.repository;
 
 import com.mercadolibre.joao_magalhaes.domain.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SectionRepository extends JpaRepository<Section, String> {
 
     Optional<Section> findByCodeAndWarehouse_Code(String sectionCode, String warehouseCode);
