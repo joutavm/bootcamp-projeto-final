431f473564f843a075bd6ad2068b6c0099bc001b
LarissaGMalagoli <larissa.malagoli@mercadolivre.com>
2021-08-06 11:06:43 -0300

    feat(us-02): refactoring product model

---
+++ src/main/java/com/mercadolibre/joao_magalhaes/domain/model/CategoryProduct.java
@@ -0,0 +1,5 @@
+package com.mercadolibre.joao_magalhaes.domain.model;
+
+public enum CategoryProduct {
+    FS, RF, FF;
+}
--- src/main/java/com/mercadolibre/joao_magalhaes/domain/model/Product.java
+++ src/main/java/com/mercadolibre/joao_magalhaes/domain/model/Product.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
@@ -18,9 +15,14 @@ public class Product {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private double price;
+    @Enumerated(EnumType.STRING)
+    private CategoryProduct category;
 
-    public Product(Long id, String name) {
+    public Product(Long id, String name, double price, CategoryProduct category) {
         this.id = id;
         this.name = name;
+        this.price = price;
+        this.category = category;
     }
 }
