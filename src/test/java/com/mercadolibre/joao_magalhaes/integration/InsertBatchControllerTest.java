14d0085840118fd79ac7eb2aa0dfef1a7253fdfd
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-08-10 14:06:50 -0300

    feat(integration_test): rafact test

--- src/test/java/com/mercadolibre/joao_magalhaes/integration/InsertBatchControllerTest.java
+++ src/test/java/com/mercadolibre/joao_magalhaes/integration/InsertBatchControllerTest.java
@@ -1,63 +1,77 @@
 package com.mercadolibre.joao_magalhaes.integration;
 
+import com.mercadolibre.joao_magalhaes.application.config.security.TokenView;
 import com.mercadolibre.joao_magalhaes.domain.dtos.form.InboundOrderForm;
+import com.mercadolibre.joao_magalhaes.domain.dtos.form.LoginForm;
+import com.mercadolibre.joao_magalhaes.domain.dtos.form.SectionForm;
+import com.mercadolibre.joao_magalhaes.domain.dtos.form.StockForm;
 import com.mercadolibre.joao_magalhaes.domain.dtos.view.StockView;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.junit.jupiter.api.BeforeEach;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InsertBatchControllerTest extends ControllerTest{
 
-    @Value("${jwt.expiration}")
-    String expiration;
 
-    @Value("${jwt.secret}")
-    String secret;
-
-    String token;
-
-
-
-    @BeforeEach
-    void setUp(){
-        Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + Long.parseLong(expiration));
-
-        token = Jwts.builder()
-                .setIssuer("Frios API")
-                .setSubject("1")
-                .setIssuedAt(now)
-                .setExpiration(expirationDate)
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
-    }
 
     @Test
-    void shouldReturnCreatedWhenPassValidForm() {
+    void shouldReturnCreatedWhenPassValidForm() throws JSONException {
+        // given
         InboundOrderForm orderForm = new InboundOrderForm();
+        SectionForm sectionForm = new SectionForm();
+        StockForm stockForm = new StockForm();
+
+        sectionForm.setSectionCode("a");
+        sectionForm.setWarehouseCode("a");
+
+        stockForm.setProductId(2L);
+        stockForm.setCurrentTemperature(50F);
+        stockForm.setMinimumTemperature(50F);
+        stockForm.setInitialQuantity(10);
+        stockForm.setCurrentQuantity(10);
+        stockForm.setManufacturingDate("03-08-2021");
+        stockForm.setManufacturingTime("05-08-2021 16:35");
+        stockForm.setDueDate("04-08-2021");
+
+        orderForm.setOrderDate("05-08-2021");
+        orderForm.setSection(sectionForm);
+        orderForm.setBatchStock(List.of(stockForm));
+
+
+
+        LoginForm login = new LoginForm();
+        login.setUserName("jorge");
+        login.setPassword("12345");
+
+
+        // When
+        ResponseEntity<TokenView> loginEntity = this.testRestTemplate.postForEntity("/auth", login, TokenView.class);
+        String token = Objects.requireNonNull(loginEntity.getBody()).getToken();
+
+
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", token);
-
+        headers.set("Authorization", "Bearer " + token);
         HttpEntity<InboundOrderForm> request = new HttpEntity<>(orderForm, headers);
 
 
-        ResponseEntity<StockView> responseEntity = this.testRestTemplate.postForEntity(
+        ResponseEntity<StockView[]> responseEntity = this.testRestTemplate.postForEntity(
                 "/api/v1/fresh-products/inboundorder/",
                 request,
-                StockView.class
+                StockView[].class
         );
 
+
+
+        // Then
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
     }
 
