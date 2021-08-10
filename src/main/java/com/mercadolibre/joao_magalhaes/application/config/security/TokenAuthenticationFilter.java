6a5e52cd736292280795c669817e95245e3b704e
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-08-10 10:13:35 -0300

    feat(security): change token name

--- src/main/java/com/mercadolibre/joao_magalhaes/application/config/security/TokenAuthenticationFilter.java
+++ src/main/java/com/mercadolibre/joao_magalhaes/application/config/security/TokenAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request){
-        String token = request.getHeader("Authorization-token");
+        String token = request.getHeader("Authorization");
         if (token == null || !token.startsWith("Bearer "))
             return null;
         return token.substring(7, token.length());
