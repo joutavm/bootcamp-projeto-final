2a41be65620120aa6f8ea33ddcd36dfd9b3920e9
mogmeli <murilo.preccaro@mercadolivre.com>
2021-08-10 15:01:12 -0300

    New Integration test

--- src/main/resources/data-integration_test.sql
+++ src/main/resources/data-integration_test.sql
@@ -10,3 +10,5 @@ insert into integrantes(name, password) values ('jorge', '$2a$10$vviww86InXz7Rgg
 insert into integrantes(name, password) values ('carlos', '$2a$10$vviww86InXz7RggmP873pubArXf05vwKGTalArLRZXEFTtD1xZ7cm');
 insert into integrantes_profiles(user_id, profiles_id) values (1, 1);
 insert into integrantes_profiles(user_id, profiles_id) values (2, 2);
+insert into stock(current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_time, minimum_temperature, product_id) values(10, 10, TO_DATE('05/08/2021', 'DD/MM/YYYY'), 2, TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('17/12/2015', 'DD/MM/YYYY'), 5, 2);
+insert into stock(current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_time, minimum_temperature, product_id) values(10, 10, TO_DATE('05/08/2021', 'DD/MM/YYYY'), 2, TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('17/12/2015', 'DD/MM/YYYY'), 5, 1);
