8721d641f8f3204c61b0e09f6b599a2d8edbcd7b
LarissaGMalagoli <larissa.malagoli@mercadolivre.com>
2021-08-10 12:02:50 -0300

    feat(us-04): adding tests for ImplFindProductInStockServiceTest

---
+++ src/test/java/com/mercadolibre/joao_magalhaes/domain/service/impl/ImplFindProductInStockServiceTest.java
@@ -0,0 +1,107 @@
+package com.mercadolibre.joao_magalhaes.domain.service.impl;
+
+import com.mercadolibre.joao_magalhaes.application.util.MockitoExtension;
+import com.mercadolibre.joao_magalhaes.domain.dtos.form.BuyProductsForm;
+import com.mercadolibre.joao_magalhaes.domain.dtos.form.StockForm;
+import com.mercadolibre.joao_magalhaes.domain.exceptions.ItemNotFoundException;
+import com.mercadolibre.joao_magalhaes.domain.model.CategoryProductEnum;
+import com.mercadolibre.joao_magalhaes.domain.model.Product;
+import com.mercadolibre.joao_magalhaes.domain.model.Stock;
+import com.mercadolibre.joao_magalhaes.domain.repository.StockRepostitory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ImplFindProductInStockServiceTest {
+
+    @Mock
+    StockRepostitory stockRepostitory;
+
+    ImplFindProductInStockService implFindProductInStockService;
+
+    @BeforeEach
+    void setUp() {
+        implFindProductInStockService = new ImplFindProductInStockService(stockRepostitory);
+    }
+
+    @Test
+    public void shouldReturnStockWhenProductIsInStock(){
+        //given
+        Product product = new Product(Long.valueOf(1), "Cheese", 2.0, CategoryProductEnum.FS);
+
+        Stock expect = new Stock(1L, product,
+                12.5f, 12.5f, 12, 12,
+                LocalDate.of(2021,3,12),
+                LocalDateTime.of(2021,3,12, 12, 30),
+                LocalDate.of(2021,3,12));
+
+        List<Stock> stockList = new ArrayList<>();
+        stockList.add(expect);
+
+        BuyProductsForm buyProductsForm = new BuyProductsForm(1L, 5);
+
+        //when
+        when(stockRepostitory.findStocksWhereIdMatchesOrderAsc(any())).thenReturn(stockList);
+        Stock result = implFindProductInStockService.findProductInStock(buyProductsForm);
+
+        //then
+        assertEquals(expect, result);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenProductNotInStocks(){
+        // given
+        Product product = new Product(Long.valueOf(1), "Cheese", 2.0, CategoryProductEnum.FS);
+
+        Stock stock = new Stock(1L, product,
+                12.5f, 12.5f, 12, 12,
+                LocalDate.of(2021,3,12),
+                LocalDateTime.of(2021,3,12, 12, 30),
+                LocalDate.of(2021,3,12));
+
+        List<Stock> stockList = new ArrayList<>();
+
+        BuyProductsForm buyProductsForm = new BuyProductsForm(1L, 50);
+
+        //when
+        when(stockRepostitory.findStocksWhereIdMatchesOrderAsc(any())).thenReturn(stockList);
+
+        // Then
+        assertThrows(ItemNotFoundException.class, () -> implFindProductInStockService.findProductInStock(buyProductsForm));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenStockNotHaveEnoughQuantity(){
+        // given
+        Product product = new Product(Long.valueOf(1), "Cheese", 2.0, CategoryProductEnum.FS);
+
+        Stock stock = new Stock(1L, product,
+                12.5f, 12.5f, 12, 12,
+                LocalDate.of(2021,3,12),
+                LocalDateTime.of(2021,3,12, 12, 30),
+                LocalDate.of(2021,3,12));
+
+        List<Stock> stockList = new ArrayList<>();
+        stockList.add(stock);
+
+        BuyProductsForm buyProductsForm = new BuyProductsForm(1L, 50);
+
+        //when
+        when(stockRepostitory.findStocksWhereIdMatchesOrderAsc(any())).thenReturn(stockList);
+
+        // Then
+        assertThrows(ItemNotFoundException.class, () -> implFindProductInStockService.findProductInStock(buyProductsForm));
+    }
+}
