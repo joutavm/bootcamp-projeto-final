3c9c49e744b03bd1fd38dce20ef7d54ccf7f2057
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-08-11 11:19:20 -0300

    feat(us_05): add method filter

--- src/main/java/com/mercadolibre/projeto_final/domain/service/DueDateService.java
+++ src/main/java/com/mercadolibre/projeto_final/domain/service/DueDateService.java
@@ -2,9 +2,10 @@ package com.mercadolibre.projeto_final.domain.service;
 
 import com.mercadolibre.projeto_final.domain.dtos.view.DueDateView;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface DueDateService {
     List<DueDateView> findByDueDate(int days);
+
+    List<DueDateView> findByDueDateSorted(int days, String category, String order);
 }
