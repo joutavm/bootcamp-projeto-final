83c9944e9238b28ebec96d81e79876ef131cbef3
LarissaGMalagoli <larissa.malagoli@mercadolivre.com>
2021-08-12 14:37:20 -0300

    feat(us-06): adding tests find all products and sort by price

--- src/test/java/com/mercadolibre/projeto_final/domain/service/impl/ImplRetrieveProductServiceTest.java
+++ src/test/java/com/mercadolibre/projeto_final/domain/service/impl/ImplRetrieveProductServiceTest.java
@@ -11,12 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import static org.mockito.ArgumentMatchers.any;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,5 +56,63 @@ class ImplRetrieveProductServiceTest {
 
         // Then
         assertEquals(expect.get(0), result.get(0));
+    }
+
+    @Test
+    void shouldReturnListOfProductsOrderByPriceAscWhenThereAreProducts(){
+        // given
+        ImplRetrieveProductService implRetrieveProductServiceSpy = Mockito.spy(implRetrieveProductService);
+
+        ProductView p1View = new ProductView("Queijo", 2.0, CategoryProductEnum.FS.toString());
+        ProductView p2View = new ProductView("Pizza", 10.0, CategoryProductEnum.FS.toString());
+        ProductView p3View = new ProductView("Presunto", 5.0, CategoryProductEnum.FS.toString());
+
+        List<ProductView> expect = new ArrayList();
+        expect.add(p1View);
+        expect.add(p3View);
+        expect.add(p2View);
+
+        List<ProductView> productList = new ArrayList();
+        productList.add(p1View);
+        productList.add(p2View);
+        productList.add(p3View);
+
+        // when
+        doReturn(productList).when(implRetrieveProductServiceSpy).retrieveProductViewList();
+        List<ProductView> result = implRetrieveProductServiceSpy.findAllSortedByPrice("asc");
+
+        // Then
+        assertEquals(expect.get(0).getName(), result.get(0).getName());
+        assertEquals(expect.get(1).getName(), result.get(1).getName());
+        assertEquals(expect.get(2).getName(), result.get(2).getName());
+    }
+
+    @Test
+    void shouldReturnListOfProductsOrderByPriceDescWhenThereAreProducts(){
+        // given
+        ImplRetrieveProductService implRetrieveProductServiceSpy = Mockito.spy(implRetrieveProductService);
+
+        ProductView p1View = new ProductView("Queijo", 2.0, CategoryProductEnum.FS.toString());
+        ProductView p2View = new ProductView("Pizza", 10.0, CategoryProductEnum.FS.toString());
+        ProductView p3View = new ProductView("Presunto", 5.0, CategoryProductEnum.FS.toString());
+
+        List<ProductView> expect = new ArrayList();
+        expect.add(p2View);
+        expect.add(p3View);
+        expect.add(p1View);
+
+        List<ProductView> productList = new ArrayList();
+        productList.add(p1View);
+        productList.add(p2View);
+        productList.add(p3View);
+
+        // when
+        doReturn(productList).when(implRetrieveProductServiceSpy).retrieveProductViewList();
+        List<ProductView> result = implRetrieveProductServiceSpy.findAllSortedByPrice("desc");
+
+        // Then
+        assertEquals(expect.get(0).getName(), result.get(0).getName());
+        assertEquals(expect.get(1).getName(), result.get(1).getName());
+        assertEquals(expect.get(2).getName(), result.get(2).getName());
     }
 }
