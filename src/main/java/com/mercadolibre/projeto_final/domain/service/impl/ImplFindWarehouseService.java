3af6bcac1db87e1e78cf2411e2b56233acf6dd6a
wagnernegrao <wagnernegrao@mercadolivre.com>
2021-08-12 12:29:55 -0300

    Add: was added service to work with warehouse

---
+++ src/main/java/com/mercadolibre/projeto_final/domain/service/impl/ImplFindWarehouseService.java
@@ -0,0 +1,42 @@
+package com.mercadolibre.projeto_final.domain.service.impl;
+
+import com.mercadolibre.projeto_final.domain.dtos.mapper.FindWarehouseMapper;
+import com.mercadolibre.projeto_final.domain.dtos.view.WarehouseStatisticsView;
+import com.mercadolibre.projeto_final.domain.exceptions.ItemNotFoundException;
+import com.mercadolibre.projeto_final.domain.model.Section;
+import com.mercadolibre.projeto_final.domain.model.Stock;
+import com.mercadolibre.projeto_final.domain.model.Warehouse;
+import com.mercadolibre.projeto_final.domain.repository.StockRepostitory;
+import com.mercadolibre.projeto_final.domain.repository.WarehouseRepository;
+import com.mercadolibre.projeto_final.domain.service.FindWarehouseService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ImplFindWarehouseService implements FindWarehouseService {
+
+    private final WarehouseRepository warehouseRepository;
+    private final StockRepostitory stockRepostitory;
+    private final FindWarehouseMapper findWarehouseMapper;
+
+
+    @Override
+    public WarehouseStatisticsView warehouseStatistics(Long id) {
+        int qtdStocks;
+
+        Warehouse warehouse = warehouseRepository
+                .findById(id)
+                .orElseThrow(() -> new ItemNotFoundException("Not Found", "Warehouse not was find", 404));
+
+        List<Stock> stocks = stockRepostitory.findAllByInboundOrder_Section_Warehouse_Id(warehouse.getId());
+
+        if (stocks.isEmpty()) qtdStocks = 0;
+
+        qtdStocks = stocks.size();
+
+        double totalSize = warehouse.getSection().stream().mapToDouble(Section::getSize).sum();
+
+        return findWarehouseMapper.map(warehouse, totalSize, qtdStocks);
+    }
+}
